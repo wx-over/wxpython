6cf2c490493535c7738b5db354b23dc0a7f594db
wangxiao <436042256@qq.com>
2019-06-25 13:53:13 +0800

    commit 1

---
+++ 课后作业.py
@@ -0,0 +1,12 @@
+#6月22日作业
+# result = 1 + 2
+# print('计算1 + 2的结果')
+# print('计算结果为：'+ str(result))
+
+# import random
+# a = random.randint(1,255)
+# b = random.randint(0,255)
+# c = random.randint(0,255)
+# d = random.randint(0,255)
+# print(str(a) + '.'+ str(b) + '.'+str(c) + '.'+str(d))
+
