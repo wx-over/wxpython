4172c310d21da47288c947482fc1a59afededf0c
wangxiao <436042256@qq.com>
2019-06-26 21:54:55 +0800

    commit 1

--- 6月24日作业.py
+++ 6月24日作业.py
@@ -2,4 +2,12 @@ import re
 
 str1 = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
 
-print(str1)
+a = re.match('(\w+)\s+([0-9a-z]+\.[0-9a-z]+\.[0-9a-z]+)\s+[A-Z]+\s+\w+/\d/\d+',str1.strip())
+abc = re.match('(\w+)\s+([0-9a-z]+\.[0-9a-z]+\.[0-9a-z]+)\s+([A-Z]+)\s+(\w+/\d/\d+)',str1.strip()).groups()
+# print(a)
+print(abc)
+
+print(f'{"VLAN ID":<15}: {abc[0]:<10}')
+print(f'{"MAC":<15}: {abc[1]:<10}')
+print(f'{"Type":<15}: {abc[2]:<10}')
+print(f'{"Interface":<15}: {abc[3]:<10}')
